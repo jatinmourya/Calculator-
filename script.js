6e0f48a227e5323dc68e02c71b6ab41fd709735d
Jatin mourya <jatinmourya07798@gmail.com>
2022-02-16 10:11:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,82 @@
+var btns = document.querySelectorAll('.btn')
+var sbtns = document.querySelectorAll('.sbtn')
+var dis = document.getElementById('display')
+var clearbtn = document.getElementById('clear')
+var eqbtn = document.getElementById('eqbtn')
+
+// clear display
+clearbtn.addEventListener('click', cleardisplay)
+
+function cleardisplay() {
+
+  //  1st way to remove last digit
+  dis.value = dis.value.slice(0, -1)
+
+
+  // 2nd way to remove last digit
+
+  // val = ''
+  // for (i = 0; i < dis.value.length - 1; i++) {
+  //   val += dis.value[i]
+  // }
+  // dis.value = val
+}
+clearbtn.addEventListener('mousedown', function () {
+  interval = setTimeout(clearall, 2000)
+  function clearall() {
+    dis.value = ''
+  }
+})
+
+// buttons onclick
+btns.forEach(function (btn) {
+  btn.addEventListener('click', addvalue)
+  function addvalue() {
+    dis.value += this.value
+  }
+
+  btn.addEventListener('mousedown', function () {
+    interr = setInterval(addvalue2, 500);
+    function addvalue2() {
+      dis.value += btn.value
+    }
+  })
+  btn.addEventListener('mouseup', function () {
+    clearInterval(interr)
+  })
+
+
+})
+
+// special buttons onclick
+sbtns.forEach(function (sbtn) {
+  sbtn.addEventListener('click', addsbtn)
+  function addsbtn() {
+
+
+    // 1st wat to find number at beginning 
+    // if (dis.value.match(/\d/)) {
+    //   cndtion = dis.value.slice(-1)
+    //   if (cndtion != '+' && cndtion != '-' && cndtion != '*') {
+    //     dis.value += this.value
+    //   }
+    // }
+
+
+
+    // 2nd wat to find number at beginning ( /[0-9]/  - RE in javascript )
+    if (dis.value.match(/[0-9]/)) {
+      cndtion = dis.value.slice(-1)
+
+      if (cndtion != '+' && cndtion != '-' && cndtion != '*' && cndtion != '.') {
+        dis.value += this.value
+      }
+    }
+  }
+})
+
+eqbtn.addEventListener('click', function () {
+  if (dis.value != '') {
+    dis.value = eval(dis.value)
+  }
+})
